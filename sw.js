c7a19447eb46538edbd143cddd7b120d1d6657e5
lielgol <lielgol@gmail.com>
2026-01-29 23:20:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = '3cars-v1';
+const cacheName = '3cars-v2';
 const assets = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
@@ -8,3 +8,4 @@ self.addEventListener('install', e => {
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
+
